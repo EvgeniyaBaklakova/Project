251f81b8cd995aaf339467492232e52940798a68
admin <stepanvill2002@yandex.ru>
2023-02-27 18:05:11 +0300

    test user and role data initialization

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -1,9 +1,33 @@
 package com.javamentor.qa.platform.service.impl;
 
+import com.javamentor.qa.platform.models.entity.user.Role;
+import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.service.abstracts.model.RoleService;
+import com.javamentor.qa.platform.service.abstracts.model.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestDataInitService {
-    public TestDataInitService() {
+
+    private final UserService userService;
+
+    private final RoleService roleService;
+
+    @Autowired
+    public TestDataInitService(UserService userService, RoleService roleService) {
+        this.roleService = roleService;
+        this.userService = userService;
+    }
+
+    public void init(){
+        Role role1 = new Role("Role_Test_1");
+        Role role2 = new Role("Role_Test_2");
+        roleService.persistAll(role1, role2);
+        userService.persist(new User(null, "email", "password", "name", null, true, false, "city", "link_site", "link_github", "link_vk", "about", "image_link", null, "nick", role1));
+        userService.persist(new User(null, "email", "password", "name", null, true, false, "city", "link_site", "link_github", "link_vk", "about", "image_link", null, "nick", role1));
+        userService.persist(new User(null, "email", "password", "name", null, true, false, "city", "link_site", "link_github", "link_vk", "about", "image_link", null, "nick", role2));
+        userService.persist(new User(null, "email", "password", "name", null, true, false, "city", "link_site", "link_github", "link_vk", "about", "image_link", null, "nick", role1));
+        userService.persist(new User(null, "email", "password", "name", null, true, false, "city", "link_site", "link_github", "link_vk", "about", "image_link", null, "nick", role2));
     }
 }
