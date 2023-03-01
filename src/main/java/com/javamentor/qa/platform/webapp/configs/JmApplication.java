b64abbc4b9a1819a2b647540aa4e4f49e54a1036
Evgeny Voinov <joekobarnd@gmail.com>
2023-03-02 01:33:10 +0300

    has a defining a bean type error with user repository add @EnableJpaRepositories in application config

--- src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
+++ src/main/java/com/javamentor/qa/platform/webapp/configs/JmApplication.java
@@ -1,14 +1,19 @@
 package com.javamentor.qa.platform.webapp.configs;
 
+import com.javamentor.qa.platform.repository.UserRepository;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @ComponentScan("com.javamentor.qa.platform")
 @EntityScan("com.javamentor.qa.platform.models.entity")
+@EnableJpaRepositories(basePackages = "com")
 public class JmApplication {
 
     public static void main(String[] args) {
