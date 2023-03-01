fc81819d3e68830fb67940fd1116b7afa061e424
Evgeny Voinov <joekobarnd@gmail.com>
2023-03-01 22:51:31 +0300

    add user repository

---
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/AuthenticationResourceController.java
@@ -0,0 +1,11 @@
+package com.javamentor.qa.platform.webapp.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth/token")
+public class AuthenticationResourceController {
+
+
+}
