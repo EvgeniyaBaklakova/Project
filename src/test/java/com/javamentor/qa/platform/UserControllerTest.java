a92de206190d20565b5a36d934b040944eea47dc
Dmitry <dimarik6977@yandex.ru>
2023-03-07 19:20:27 +0300

    MockMvc V_1

--- src/test/java/com/javamentor/qa/platform/UserControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/UserControllerTest.java
@@ -2,17 +2,13 @@ package com.javamentor.qa.platform;
 
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.hamcrest.Matchers.notNullValue;
-import static org.hamcrest.core.Is.is;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class UserControllerTest extends AbstractTestApi {
 
