dd5262c4c39244f79f4275107fce6a177ac539d7
Dmitry <dimarik6977@yandex.ru>
2023-03-07 19:35:31 +0300

    MockMvc V_1

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourceControllerTest.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/UserResourceControllerTest.java
@@ -4,14 +4,16 @@ import com.javamentor.qa.platform.models.dto.UserDtoTest;
 import com.javamentor.qa.platform.service.abstracts.dto.UserDtoTestService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/test")
 public class UserResourceControllerTest {
 
-private final UserDtoTestService userDtoTestService;
+    private final UserDtoTestService userDtoTestService;
 
     public UserResourceControllerTest(UserDtoTestService userDtoTestService) {
         this.userDtoTestService = userDtoTestService;
