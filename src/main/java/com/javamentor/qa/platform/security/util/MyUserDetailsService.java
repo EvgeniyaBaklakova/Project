a432882d49077362fde10c9ccc401bce7bcdb8aa
Evgeny Voinov <joekobarnd@gmail.com>
2023-03-12 17:17:51 +0300

    add user details service

---
+++ src/main/java/com/javamentor/qa/platform/security/util/MyUserDetailsService.java
@@ -0,0 +1,31 @@
+package com.javamentor.qa.platform.security.util;
+
+import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Optional;
+
+@Component
+public class MyUserDetailsService implements UserDetailsService {
+
+    @Autowired private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> userRes = userRepository.getUserByLogin(email);
+        if(userRes.isEmpty())
+            throw new UsernameNotFoundException("Could not findUser with email = " + email);
+        User user = userRes.get();
+        return new org.springframework.security.core.userdetails.User(
+                email,
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+    }
+}
