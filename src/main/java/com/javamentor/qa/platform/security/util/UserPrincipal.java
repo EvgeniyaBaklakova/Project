af5c41c9b16ede6a369e9652a20fce11b97ebb3b
Evgeny Voinov <joekobarnd@gmail.com>
2023-03-13 15:13:34 +0300

    v4

---
+++ src/main/java/com/javamentor/qa/platform/security/util/UserPrincipal.java
@@ -0,0 +1,68 @@
+package com.javamentor.qa.platform.security.util;
+
+import com.javamentor.qa.platform.models.entity.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserPrincipal extends User {
+
+    private static final long serialVersionUID = 1L;
+    private String email;
+    private String password;
+    private boolean enabled;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    public void setUsername(String username) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    public void setAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        this.authorities = authorities;
+    }
+}
+    
