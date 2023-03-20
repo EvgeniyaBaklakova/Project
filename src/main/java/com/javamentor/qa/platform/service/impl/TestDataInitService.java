97b77550d41d28fefd873def081e3b321707e21a
Evgeny Voinov <joekobarnd@gmail.com>
2023-03-20 19:10:03 +0300

    return role and user service

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -1,9 +1,20 @@
 package com.javamentor.qa.platform.service.impl;
 
+import com.javamentor.qa.platform.models.entity.user.Role;
+import com.javamentor.qa.platform.service.abstracts.model.RoleService;
+import com.javamentor.qa.platform.service.abstracts.model.UserService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestDataInitService {
     public TestDataInitService() {
+
+        final UserService userService;
+
+        final RoleService roleService;
+
+        final Role ROLE_USER = new Role("ROLE_USER");
+        final Role ROLE_ADMIN = new Role("ROLE_ADMIN");
+
     }
 }
