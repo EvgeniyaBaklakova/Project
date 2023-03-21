ccd3d70797da23445ca7ce40e93dcbd1dd5ba49e
Evgeny Voinov <joekobarnd@gmail.com>
2023-03-21 17:57:18 +0300

    fix user details service

--- src/main/java/com/javamentor/qa/platform/security/util/UserDetailServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/security/util/UserDetailServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 
 @Service
 public class UserDetailServiceImpl implements UserDetailsService {
@@ -20,10 +19,10 @@ public class UserDetailServiceImpl implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User userDetails = userRepository.loadUserByUsername(username);
-        if (userDetails == null) {
+        User user = userRepository.loadUserByUsername(username);
+        if (user == null) {
             throw new UsernameNotFoundException(username);
         }
-        return new org.springframework.security.core.userdetails.User(userDetails.getUsername(), userDetails.getPassword(), new ArrayList<>());
+        return user;
     }
 }
