c9ff2a291809b0f072bde4f4b655f2651618f142
kesh9 <kesh.92@yandex.ru>
2023-03-22 20:49:51 +0300

    init question, tag and answer

--- src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/TestDataInitService.java
@@ -1,11 +1,18 @@
 package com.javamentor.qa.platform.service.impl;
 
+import com.javamentor.qa.platform.models.entity.question.Question;
+import com.javamentor.qa.platform.models.entity.question.Tag;
+import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 import com.javamentor.qa.platform.models.entity.user.Role;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.model.RoleService;
-import com.javamentor.qa.platform.service.abstracts.model.UserService;
+import com.javamentor.qa.platform.service.abstracts.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.Clock;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class TestDataInitService {
@@ -14,13 +21,20 @@ public class TestDataInitService {
 
     private final RoleService roleService;
 
+    private final QuestionService questionService;
+    private final TagService tagService;
+    private final AnswerService answerService;
+
     private final Role ROLE_USER = new Role("ROLE_USER");
     private final Role ROLE_ADMIN = new Role("ROLE_ADMIN");
 
     @Autowired
-    public TestDataInitService(UserService userService, RoleService roleService) {
+    public TestDataInitService(UserService userService, RoleService roleService, QuestionService questionService, TagService tagService, AnswerService answerService) {
         this.roleService = roleService;
         this.userService = userService;
+        this.questionService = questionService;
+        this.tagService = tagService;
+        this.answerService = answerService;
     }
 
     public void initRoles() {
@@ -29,9 +43,50 @@ public class TestDataInitService {
 
     public void initUsers() {
         for(int i = 1; i <= 100; i = i + 2) {
-            userService.persistAll(new User(null, "email" + i + "@mail.com", "password", "name", null, true, false, "city", "link_site", "link_github", "link_vk", "about", "image_link", null, "nick", ROLE_USER),
-                    new User(null, "email" + (i + 1) + "@mail.com", "password", "name", null, true, false, "city", "link_site", "link_github", "link_vk", "about", "image_link", null, "nick", ROLE_ADMIN));
+            userService.persistAll(new User(null, "email" + i + "@mail.com", "password", "name",
+                            null, true, false, "city", "link_site", "link_github",
+                            "link_vk", "about", "image_link", null, "nick", ROLE_USER),
+                    new User(null, "email" + (i + 1) + "@mail.com", "password", "name", null,
+                            true, false, "city", "link_site", "link_github", "link_vk",
+                            "about", "image_link", null, "nick", ROLE_ADMIN));
         }
+    }
+
+    public void initTag() {
+        for (int i = 1; i < 50; i++) {
+            Tag tag = new Tag();
+            tag.setName("Name" + i);
+            tag.setDescription("Description" + i);
+            tag.setPersistDateTime(LocalDateTime.now(Clock.systemDefaultZone()));
+            tagService.persistAll(tag);
+        }
+    }
+
+    public void initQuestion() {
+        Question question = new Question();
+        question.setTitle("Title");
+        question.setDescription("Description");
+        question.setLastUpdateDateTime(LocalDateTime.now(Clock.systemDefaultZone()));
+        question.setUser(userService.getAll().get(1));
+        question.setIsDeleted(false);
+        List<Tag> tagList = new ArrayList<>();
+        tagList.add(tagService.getAll().get(0));
+        question.setTags(tagList);
+        questionService.persistAll(question);
+    }
+
+    public void initAnswer() {
+        Answer answer = new Answer();
+        answer.setDateAcceptTime(LocalDateTime.now(Clock.systemDefaultZone()));
+        answer.setHtmlBody("html body");
+        answer.setIsDeleted(false);
+        answer.setIsDeletedByModerator(false);
+        answer.setIsHelpful(true);
+        answer.setPersistDateTime(LocalDateTime.now(Clock.systemDefaultZone()));
+        answer.setUpdateDateTime(LocalDateTime.now(Clock.systemDefaultZone()));
+        answer.setQuestion(questionService.getAll().get(1));
+        answer.setUser(userService.getAll().get(1));
+        answerService.persistAll(answer);
     }
 
 }
