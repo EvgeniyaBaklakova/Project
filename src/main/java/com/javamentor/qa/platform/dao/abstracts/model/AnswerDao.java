860f9c238c17b84046cb6787936405bcc612a09c
Dmitry <dimarik6977@yandex.ru>
2023-03-22 09:13:34 +0300

    ApiAnswerDelete

--- src/main/java/com/javamentor/qa/platform/dao/abstracts/model/AnswerDao.java
+++ src/main/java/com/javamentor/qa/platform/dao/abstracts/model/AnswerDao.java
@@ -3,6 +3,6 @@ package com.javamentor.qa.platform.dao.abstracts.model;
 import com.javamentor.qa.platform.models.entity.question.answer.Answer;
 
 public interface AnswerDao extends ReadWriteDao<Answer, Long> {
-    void deleteById (Long answerId);
+
 
 }
