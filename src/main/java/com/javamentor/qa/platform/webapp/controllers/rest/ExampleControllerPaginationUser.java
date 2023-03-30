0f10a375501bf340a7510e1c369fb55851318fc2
igor romanov <predator.1199@mail.ru>
2023-03-30 21:10:53 +0700

    init

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ExampleControllerPaginationUser.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ExampleControllerPaginationUser.java
@@ -11,7 +11,10 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
