26eea3e9fd852790d9405720d30ec9c855657ef5
Evgeny Voinov <joekobarnd@gmail.com>
2023-04-12 08:55:13 +0300

    bd fix

--- src/main/java/com/javamentor/qa/platform/security/service/UserDetailsServiceImpl.java
+++ src/main/java/com/javamentor/qa/platform/security/service/UserDetailsServiceImpl.java
@@ -3,11 +3,13 @@ package com.javamentor.qa.platform.security.service;
 import com.javamentor.qa.platform.dao.abstracts.model.UserDao;
 import com.javamentor.qa.platform.models.entity.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.Optional;
 
 
@@ -24,8 +26,26 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 
-        Optional<User> user = userDao.getUserByEmail(email);
-        return user.orElseThrow();
+        // Fetch User from the DB
+        Optional<User> userRes = userDao.getUserByEmail(email);
+        // No user found
+        if(userRes.isEmpty())
+            throw new UsernameNotFoundException("Could not findUser with email = {}" + email);
+        // Return a User Details object using the fetched User information
+        User user = userRes.get();
+        return new org.springframework.security.core.userdetails.User(
+                email,
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER"))); // Sets the role of the found user
+
+
+
+
+
+
+
+//        Optional<User> user = userDao.getUserByEmail(email);
+//        return user.orElseThrow();
 
     }
 }
