cff3e2ddccf58a1ab6c4893593a7518674d4c6d0
kesh9 <kesh.92@yandex.ru>
2023-04-21 13:58:51 +0300

    ignored_tags

--- src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/dto/TagDtoDaoImpl.java
@@ -1,6 +1,7 @@
 package com.javamentor.qa.platform.dao.impl.dto;
 
 import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
+import com.javamentor.qa.platform.models.dto.tag.IgnoredTagsDto;
 import com.javamentor.qa.platform.models.dto.tag.RelatedTagsDto;
 import org.springframework.stereotype.Repository;
 
@@ -20,5 +21,12 @@ public class TagDtoDaoImpl implements TagDtoDao {
                 "(SELECT COUNT(q.id) FROM Question q JOIN q.tags as qt WHERE qt.id = t.id) as c)" +
                 " FROM Tag t ORDER BY c DESC";
         return entityManager.createQuery(hql).setMaxResults(10).getResultList();
+    }
+
+    @Override
+    public List<IgnoredTagsDto> getIgnoredTags(Long userId) {
+        String hql = "SELECT NEW com.javamentor.qa.platform.models.dto.tag.IgnoredTagsDto(t.id, t.ignoredTag.name" +
+                "FROM IgnoredTag t WHERE t.user.id = :userId";
+        return entityManager.createQuery(hql).setParameter("userId", userId).getResultList();
     }
 }
--- src/main/java/com/javamentor/qa/platform/service/abstracts/dto/TagDtoService.java
+++ src/main/java/com/javamentor/qa/platform/service/abstracts/dto/TagDtoService.java
@@ -1,9 +1,11 @@
 package com.javamentor.qa.platform.service.abstracts.dto;
 
+import com.javamentor.qa.platform.models.dto.tag.IgnoredTagsDto;
 import com.javamentor.qa.platform.models.dto.tag.RelatedTagsDto;
 
 import java.util.List;
 
 public interface TagDtoService {
     List<RelatedTagsDto> getAllTen();
+    List<IgnoredTagsDto> getIgnoredTags(Long userId);
 }
