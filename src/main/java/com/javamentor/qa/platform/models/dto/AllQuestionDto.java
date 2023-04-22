a9f938fd798496fb86f4218d9a878fff4f428d5c
Александр <dpn1500@gmail.com>
2023-04-22 10:54:53 +0300

    v 1.0

--- src/main/java/com/javamentor/qa/platform/models/dto/AllQuestionDto.java
+++ src/main/java/com/javamentor/qa/platform/models/dto/AllQuestionDto.java
@@ -5,12 +5,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class  AllQuestionDto  {
-    private Long id;
-    private String title;
+    private Long questionId;
+    private String questionTitle;
+    private LocalDateTime answerPersistDateTime;
+//    private Long countAnswer;
+
 
 }
