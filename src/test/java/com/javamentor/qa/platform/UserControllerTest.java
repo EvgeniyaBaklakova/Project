20cca52bac87284a74a093cabddfabea23c796c7
Александр <dpn1500@gmail.com>
2023-05-03 14:18:25 +0300

    test

--- src/test/java/com/javamentor/qa/platform/UserControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/UserControllerTest.java
@@ -2,12 +2,17 @@ package com.javamentor.qa.platform;
 
 import org.hamcrest.core.Is;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.http.HttpHeaders.AUTHORIZATION;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -33,6 +38,36 @@ public class UserControllerTest extends AbstractTestApi {
                 .andExpect(jsonPath("$.city", Is.is("Saint-Petersburg")));
 
     }
+
+    @Test
+    @Sql(scripts = "/script/TestUserResourceController/Before1.sql", executionPhase = BEFORE_TEST_METHOD)
+    @Sql(scripts = "/script/TestUserResourceController/After1.sql", executionPhase = AFTER_TEST_METHOD)
+    public void getCountAnswers() throws Exception {
+
+        System.out.println("START TEST");
+
+        MvcResult result = mvc.perform(MockMvcRequestBuilders
+                        .post("/api/auth/token")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"email\" : \"test100@mail.ru\", \"password\" : \"password\"}")) //тут вставляете ваши данные
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String response = result.getResponse().getContentAsString();
+        String token = response.replace("{\"jwtToken\":\"", "").replace("\"}", "");
+        System.out.println(token);
+
+
+        this.mvc.perform(MockMvcRequestBuilders
+                        .get("/api/user/profile/question/week")
+                        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$",Is.is(2)));
+    }
+
+
+
 
 
 }
