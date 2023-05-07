2a0eb60500326faf97979098e28ae3ed07c259db
Evgeny Voinov <joekobarnd@gmail.com>
2023-05-07 17:05:06 +0300

    no

---
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/AdminResourceController.java
@@ -0,0 +1,33 @@
+package com.javamentor.qa.platform.webapp.controllers.rest;
+
+
+
+import com.javamentor.qa.platform.models.entity.user.User;
+import com.javamentor.qa.platform.service.abstracts.model.UserService;
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
+
+@Api(value = "admin resource controller", tags = "блокировка пользователя по id")
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/admin")
+public class AdminResourceController {
+
+    private final UserService userService;
+
+    @PostMapping(value = "/block")
+    public void blockUser(@RequestParam Long userId) {
+        Optional<User> user = userService.getById(userId);
+        user.ifPresent(u -> {
+                    u.setIsEnabled(false);
+                    userService.update(u);
+                }
+        );
+    }
+}
---
+++ src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/AuthenticationResourceControllerTest.java
@@ -0,0 +1,15 @@
+package com.javamentor.qa.platform.webapp.controllers.rest;
+
+import com.javamentor.qa.platform.AbstractTestApi;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AuthenticationResourceControllerTest extends AbstractTestApi {
+
+
+
+    @Test
+    void authenticateUser() {
+    }
+}
