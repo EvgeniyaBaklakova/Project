c91488cf9453be7d4620e6bac129f1ffbaf25b55
Александр <dpn1500@gmail.com>
2023-05-07 09:23:33 +0300

    06/05/2023

--- src/test/java/com/javamentor/qa/platform/api/AnswerTestController/AnswerResourceControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/api/AnswerTestController/AnswerResourceControllerTest.java
@@ -14,8 +14,6 @@ import java.util.Map;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
