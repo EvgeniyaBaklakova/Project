e9e892261fd497235458bcaeb5ebe395bc1ec9ae
Evgeny Voinov <joekobarnd@gmail.com>
2023-05-08 22:06:23 +0300

    st

--- src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/AdminResourceControllerTest.java
+++ src/test/java/com/javamentor/qa/platform/webapp/controllers/rest/AdminResourceControllerTest.java
@@ -8,32 +8,35 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.client.RestClientTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.AFTER_TEST_METHOD;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
 
 class AdminResourceControllerTest extends AbstractTestApi {
 
     @Autowired
-    protected UserService userService;
-    @Autowired
-    protected TestRestTemplate testRestTemplate;
+    private UserService userService;
 
-    private final Long USER_ID = 102L;
+    private final Long USER_ID = 101L;
 
     @DisplayName("set isEnable as false")
     @Sql(value = "/script/AuthController/Before.sql", executionPhase = BEFORE_TEST_METHOD)
     @Sql(value = "/script/AuthController/After.sql", executionPhase = AFTER_TEST_METHOD)
     @Test
-    void testBlockUserController() {
+    void testBlockUserController() throws Exception {
 
-        String url = "/api/admin/block?userId=" + USER_ID;
-        testRestTemplate.postForEntity(url, null, Void.class);
+        String url = "/api/admin/block?userId=101";
 
-        // Проверяем, что пользователь заблокирован
+        mvc.perform(post(url));
         User blockedUser = userService.getById(USER_ID).orElse(null);
+        userService.update(blockedUser);
         Assertions.assertNotNull(blockedUser);
         Assertions.assertFalse(blockedUser.getIsEnabled());
     }
