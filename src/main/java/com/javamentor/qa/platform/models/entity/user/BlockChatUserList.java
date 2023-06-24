2df0728c76622273f4d3ca0de2a009bea66fd2be
Hutzy <whodoctor553@gmail.com>
2023-06-25 00:11:26 +0300

    add_QuestionDtoByTagId

---
+++ src/main/java/com/javamentor/qa/platform/models/entity/user/BlockChatUserList.java
@@ -0,0 +1,54 @@
+package com.javamentor.qa.platform.models.entity.user;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "block_chat_user_list")
+public class BlockChatUserList implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "BlockChatUserList_seq")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_profile")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "user_blocked")
+    private User isBlocked;
+
+    @CreationTimestamp
+    @Column(name = "persist_date", updatable = false)
+    @Type(type = "org.hibernate.type.LocalDateTimeType")
+    private LocalDateTime persistDate;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BlockChatUserList that = (BlockChatUserList) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
