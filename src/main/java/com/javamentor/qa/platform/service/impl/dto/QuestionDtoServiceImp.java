8ea4a2dc17197c7c7f998a821480cd3af16c50bd
Antony <mr.zlobin2015@yandex.ru>
2023-06-26 17:58:49 +0300

    refactor QuestionDtoServiceImp

--- src/main/java/com/javamentor/qa/platform/service/impl/dto/QuestionDtoServiceImp.java
+++ src/main/java/com/javamentor/qa/platform/service/impl/dto/QuestionDtoServiceImp.java
@@ -3,8 +3,6 @@ package com.javamentor.qa.platform.service.impl.dto;
 import com.javamentor.qa.platform.dao.abstracts.dto.QuestionDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.dto.TagDtoDao;
 import com.javamentor.qa.platform.dao.abstracts.pagination.PageDtoDao;
-import com.javamentor.qa.platform.exception.NoSuchDaoException;
-import com.javamentor.qa.platform.exception.PageException;
 import com.javamentor.qa.platform.models.dto.PageDto;
 import com.javamentor.qa.platform.models.dto.UserProfileQuestionDto;
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
@@ -12,24 +10,23 @@ import com.javamentor.qa.platform.models.dto.tag.TagDto;
 import com.javamentor.qa.platform.models.entity.pagination.PaginationData;
 import com.javamentor.qa.platform.models.entity.question.TagQuestion;
 import com.javamentor.qa.platform.service.abstracts.dto.QuestionDtoService;
-import org.apache.commons.collections.map.MultiValueMap;
-import org.apache.poi.ss.formula.functions.T;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.*;
+import java.util.List;
+import java.util.Optional;
+import java.util.Map;
+import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 @Service
-public class QuestionDtoServiceImp implements QuestionDtoService {
+public class QuestionDtoServiceImp extends PageDtoServiceImpl<QuestionDto> implements QuestionDtoService {
     private final TagDtoDao tagDtoDao;
     private final QuestionDtoDao questionDtoDao;
 
-    private final Map<String, PageDtoDao<QuestionDto>> daoMap;
-
 
     public QuestionDtoServiceImp(TagDtoDao tagDtoDao, QuestionDtoDao questionDtoDao, Map<String, PageDtoDao<QuestionDto>> daoMap) {
-        this.daoMap = daoMap;
+        super(daoMap);
         this.tagDtoDao = tagDtoDao;
         this.questionDtoDao = questionDtoDao;
 
@@ -70,47 +67,18 @@ public class QuestionDtoServiceImp implements QuestionDtoService {
 
     @Override
     public PageDto<QuestionDto> getPageDto(PaginationData properties) {
-        if (!daoMap.containsKey(properties.getDaoName())) {
-            throw new NoSuchDaoException("There is no dao with name: " + properties.getDaoName());
-        }
-        if (properties.getCurrentPage() < 0) {
-            throw new PageException("The number can`t be less than 0");
-        }
-        if (properties.getItemsOnPage() < 0) {
-            throw new PageException("The number can`t be less than 0");
-        }
-        PageDtoDao<QuestionDto> currentDao = daoMap.get(properties.getDaoName());
-        List<QuestionDto> items = currentDao.getItems(properties);
+        PageDto<QuestionDto> pageDto = super.getPageDto(properties);
+        List<QuestionDto> items = pageDto.getItems();
         List<Long> ids = new ArrayList<>();
-
-        for (QuestionDto questionDto:items
-             ) {
-            ids.add(questionDto.getId());
-        }
-
-        MultiValueMap tagMap = new MultiValueMap();
-
+        items.forEach(item -> ids.add(item.getId()));
         List<TagQuestion> tagQuestions = tagDtoDao.getTagsByQuestionsIds(ids);
-
-        for (TagQuestion tagQuestion:tagQuestions
-             ) {
-                tagMap.put(tagQuestion.getQuestionId(), tagQuestion.getTagDto());
-        }
-
-        for (QuestionDto questionDto:items
-        ) {
-            questionDto.setListTagDto((List<TagDto>) tagMap.get(questionDto.getId()));
-        }
-
-        PageDto<QuestionDto> pageDTO = new PageDto<>();
-        pageDTO.setCurrentPageNumber(properties.getCurrentPage());
-        pageDTO.setItems(items);
-        pageDTO.setItemsOnPage(pageDTO.getItems().size());
-        pageDTO.setTotalResultCount(Math.toIntExact((Long) currentDao.getTotalResultCount(properties.getProps())));
-        pageDTO.setTotalPageCount((int) Math.ceil((double) pageDTO.getTotalResultCount() / properties.getItemsOnPage()));
-        return pageDTO;
+        Map<Long, List<TagDto>> tagMap = tagQuestions.stream()
+                .collect(Collectors.groupingBy(TagQuestion::getQuestionId,
+                        Collectors.mapping(TagQuestion::getTagDto, Collectors.toList())));
+        items.forEach(item -> item.setListTagDto(tagMap.get(item.getId())));
+        pageDto.setItems(items);
+        return pageDto;
     }
-
 
 }
 
