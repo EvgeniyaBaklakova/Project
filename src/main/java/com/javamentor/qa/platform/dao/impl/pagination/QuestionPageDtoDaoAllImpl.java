fc7d05c14aff5189c01c7cccdff688c7744fb1dd
Vlad Tkachenko <tkachenko.vladik88@gmail.com>
2023-06-28 23:51:06 +0300

    code to read fixed

--- src/main/java/com/javamentor/qa/platform/dao/impl/pagination/QuestionPageDtoDaoAllImpl.java
+++ src/main/java/com/javamentor/qa/platform/dao/impl/pagination/QuestionPageDtoDaoAllImpl.java
@@ -4,6 +4,7 @@ import com.javamentor.qa.platform.dao.abstracts.pagination.PageDtoDao;
 import com.javamentor.qa.platform.dao.impl.pagination.transformers.QuestionDtoResultTransformer;
 import com.javamentor.qa.platform.models.dto.question.QuestionDto;
 import com.javamentor.qa.platform.models.entity.pagination.PaginationData;
+import org.hibernate.transform.ResultTransformer;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
@@ -18,26 +19,57 @@ public class QuestionPageDtoDaoAllImpl implements PageDtoDao<QuestionDto> {
     @PersistenceContext
     private EntityManager entityManager;
 
-
     @Override
     public List<QuestionDto> getItems(PaginationData properties) {
-        List<String> trackedTag = (List<String>) properties.getProps().getOrDefault("trackedTag", null);
-        List<String> ignoredTag = (List<String>) properties.getProps().getOrDefault("ignoredTag", null);
+        List<String> trackedTag = (List<String>)
+                properties.getProps().getOrDefault("trackedTag", null);
+        List<String> ignoredTag = (List<String>)
+                properties.getProps().getOrDefault("ignoredTag", null);
+
         int items = properties.getItemsOnPage();
         int offset = (properties.getCurrentPage() - 1) * items;
 
+        String hql =
+                "SELECT q.id AS q_id, " +
+                "q.title AS q_title, " +
+                "q.user.id AS user_id, " +
 
-        String hql = "SELECT q.id as q_id, q.title as q_title, q.user.id as user_id," +
-                "cast((Select r.count FROM Reputation r JOIN r.author as ra Where ra.id = q.user.id) as java.lang.Long) as rep," +
-                " q.user.fullName as u_name, q.user.imageLink as img, q.description as desc," +
-                "(select count(qv.id) from QuestionViewed qv JOIN qv.question as qvq WHERE qvq.id = q.id) as vc," +
-                "(select count(a.id) from Answer a JOIN a.question as aq WHERE aq.id = q.id) as ac," +
-                "(select count(vq.id) from VoteQuestion vq JOIN vq.question as vqq WHERE vqq.id = q.id) as valc," +
-                "q.persistDateTime as pdt, q.lastUpdateDateTime as udt FROM Question q" +
-                " WHERE q.id in (select q.id from Question q JOIN q.tags as qt WHERE" +
-                " :tt is null or qt.name in (:tt)) and q.id not in (select q.id from Question q JOIN q.tags as qt WHERE" +
-                " qt.name in (:it))";
+                "CAST((SELECT r.count " +
+                "FROM Reputation r " +
+                "JOIN r.author AS ra " +
+                "WHERE ra.id = q.user.id) AS java.lang.Long) AS rep, " +
 
+                "q.user.fullName AS u_name, " +
+                "q.user.imageLink AS img, " +
+                "q.description AS desc," +
+
+                "(SELECT COUNT(qv.id) " +
+                "FROM QuestionViewed qv " +
+                "JOIN qv.question AS qvq " +
+                "WHERE qvq.id = q.id) AS vc," +
+
+                "(SELECT count(a.id) " +
+                "FROM Answer a " +
+                "JOIN a.question AS aq " +
+                "WHERE aq.id = q.id) AS ac, " +
+
+                "(SELECT COUNT(vq.id) " +
+                 "FROM VoteQuestion vq " +
+                 "JOIN vq.question AS vqq " +
+                 "WHERE vqq.id = q.id) AS valc, " +
+
+                "q.persistDateTime AS pdt, " +
+                "q.lastUpdateDateTime AS udt " +
+
+                "FROM Question q " +
+                "WHERE q.id IN (SELECT q.id " +
+                                "FROM Question q " +
+                                "JOIN q.tags AS qt " +
+                                "WHERE :tt IS NULL OR " +
+                    "qt.name IN (:tt)) AND q.id NOT IN (SELECT q.id " +
+                                                        "FROM Question q " +
+                                                        "JOIN q.tags AS qt " +
+                                                        "WHERE qt.name IN (:it))";
 
         Query query = entityManager.createQuery(hql)
                 .setFirstResult(offset)
@@ -48,18 +80,22 @@ public class QuestionPageDtoDaoAllImpl implements PageDtoDao<QuestionDto> {
                 .setResultTransformer(new QuestionDtoResultTransformer());
 
         return query.getResultList();
-
     }
 
     @Override
     public Long getTotalResultCount(Map<String, Object> properties) {
-        String hql = "select count(q.id) from Question q " +
-                "WHERE q.id in (select q.id from Question q JOIN q.tags as qt WHERE" +
-                " :tt is null or qt.name in (:tt)) and q.id not in (select q.id from Question q JOIN q.tags as qt WHERE" +
-                " qt.name in (:it))";
+        String hql = "SELECT COUNT(q.id) FROM Question q " +
+                 "WHERE q.id IN (SELECT q.id " +
+                                "FROM Question q " +
+                                "JOIN q.tags AS qt " +
+                                "WHERE :tt IS NULL OR qt.name IN (:tt)) " +
+                                "AND q.id NOT IN (SELECT q.id " +
+                                                 "FROM Question q " +
+                                                 "JOIN q.tags AS qt " +
+                                                 "WHERE qt.name IN (:it))";
+
         List<String> trackedTag = (List<String>) properties.get("trackedTag");
         List<String> ignoredTag = (List<String>) properties.get("ignoredTag");
-
 
         Query query = entityManager.createQuery(hql)
                 .setParameter("tt", trackedTag)
