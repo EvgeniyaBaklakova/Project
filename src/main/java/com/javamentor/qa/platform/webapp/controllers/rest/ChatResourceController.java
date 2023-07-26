5a79c87ea9169a7706712ab69793c6ebcf49a085
text-code <dssn2102@gmail.com>
2023-07-27 07:01:41 +0900

    add getSingleChatDto() to ChatResourceController - fix replace GroupChatDtoService with ChatDtoService - fix replace GroupChatDtoDao with ChatDtoDao

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ChatResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ChatResourceController.java
@@ -2,8 +2,9 @@ package com.javamentor.qa.platform.webapp.controllers.rest;
 
 import com.javamentor.qa.platform.models.dto.chat.ChatDto;
 import com.javamentor.qa.platform.models.dto.chat.GroupChatDto;
+import com.javamentor.qa.platform.models.dto.chat.SingleChatDto;
 import com.javamentor.qa.platform.models.entity.user.User;
-import com.javamentor.qa.platform.service.abstracts.dto.GroupChatDtoService;
+import com.javamentor.qa.platform.service.abstracts.dto.ChatDtoService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
@@ -20,24 +21,31 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/user/chat")
 public class ChatResourceController {
-    private final GroupChatDtoService groupChatDtoService;
+    private final ChatDtoService chatDtoService;
 
-    public ChatResourceController(GroupChatDtoService groupChatDtoService) {
-        this.groupChatDtoService = groupChatDtoService;
+    public ChatResourceController(ChatDtoService chatDtoService) {
+        this.chatDtoService = chatDtoService;
     }
 
     @GetMapping
     @ApiOperation(value = "Поиск чатов по имени")
     public ResponseEntity<List<ChatDto>> getChatDtoByChatName(@RequestParam String chatName,
                                                               @AuthenticationPrincipal User user) {
-        List<ChatDto> result = groupChatDtoService.getChatDtoByChatName(chatName, user.getId());
+        List<ChatDto> result = chatDtoService.getChatDtoByChatName(chatName, user.getId());
+        return new ResponseEntity<>(result, HttpStatus.OK);
+    }
+
+    @GetMapping("/single")
+    @ApiOperation(value = "Получение всех личных чатов")
+    public ResponseEntity<List<SingleChatDto>> getSingleChatDto(@AuthenticationPrincipal User user) {
+        List<SingleChatDto> result = chatDtoService.getSingleChatDtoByUserId(user.getId());
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
     @GetMapping("/group")
     @ApiOperation(value = "Получение всех групповых чатов")
     public ResponseEntity<List<GroupChatDto>> getGroupChatDto(@AuthenticationPrincipal User user) {
-        List<GroupChatDto> result = groupChatDtoService.getGroupChatDtoByUserId(user.getId());
+        List<GroupChatDto> result = chatDtoService.getGroupChatDtoByUserId(user.getId());
         return new ResponseEntity<>(result, HttpStatus.OK);
     }
 }
--- src/test/java/com/javamentor/qa/platform/api/TestChatResourceController.java
+++ src/test/java/com/javamentor/qa/platform/api/TestChatResourceController.java
@@ -16,54 +16,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 public class TestChatResourceController extends AbstractTestApi {
     @Test
-    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDto/Before.sql",
-            executionPhase = BEFORE_TEST_METHOD)
-    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDto/After.sql",
-            executionPhase = AFTER_TEST_METHOD)
-    public void getGroupChatDto() throws Exception {
-
-        String USER_TOKEN = getToken("test101@mail.ru", "123");
-
-        this.mvc.perform(get("/api/user/chat/group")
-                        .header(AUTHORIZATION, USER_TOKEN)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$[0].id", Is.is(111)))
-                .andExpect(jsonPath("$[0].chatName", Is.is("first_chat")))
-                .andExpect(jsonPath("$[0].lastMessage", Is.is("2023-01-03_last_message_first_chat")))
-                .andExpect(jsonPath("$[0].image", Is.is("first_image")))
-                .andExpect(jsonPath("$[0].persistDateTime", Is.is("2000-01-01T00:00:00")))
-
-                .andExpect(jsonPath("$[1].id", Is.is(112)))
-                .andExpect(jsonPath("$[1].chatName", Is.is("second_chat")))
-                .andExpect(jsonPath("$[1].lastMessage", Is.is("2023-01-05_last_message_second_chat")))
-                .andExpect(jsonPath("$[1].image", Is.is("second_image")))
-                .andExpect(jsonPath("$[1].persistDateTime", Is.is("2000-02-01T00:00:00")))
-
-                .andExpect(jsonPath("$.length()", Is.is(2)));
-    }
-
-    @Test
-    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDtoEmpty/Before.sql",
-            executionPhase = BEFORE_TEST_METHOD)
-    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDtoEmpty/After.sql",
-            executionPhase = AFTER_TEST_METHOD)
-    public void getGroupChatDtoEmpty() throws Exception {
-
-        String USER_TOKEN = getToken("test101@mail.ru", "123");
-
-        this.mvc.perform(get("/api/user/chat/group")
-                        .header(AUTHORIZATION, USER_TOKEN)
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andDo(print())
-                .andExpect(status().isOk())
-                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
-                .andExpect(jsonPath("$").isEmpty());
-    }
-
-    @Test
     @ApiOperation(value = "Проверка на возвращение чатов (single и group) по имени. С сортировкой по дате")
     @Sql(scripts = "/script/TestChatResourceController/TestGetChatDtoByName/Before.sql",
             executionPhase = BEFORE_TEST_METHOD)
@@ -88,7 +40,7 @@ public class TestChatResourceController extends AbstractTestApi {
 
                 .andExpect(jsonPath("$[1].id", Is.is(114)))
                 .andExpect(jsonPath("$[1].name", Is.is("second")))
-                .andExpect(jsonPath("$[1].image", Is.is("No link")))
+                .andExpect(jsonPath("$[1].image", Is.is("second_user_image")))
                 .andExpect(jsonPath("$[1].lastMessage", Is.is("2023-02-11_last_message_four_chat")))
                 .andExpect(jsonPath("$[1].persistDateTimeLastMessage", Is.is("2023-02-11T00:00:00")))
 
@@ -134,6 +86,106 @@ public class TestChatResourceController extends AbstractTestApi {
         this.mvc.perform(get("/api/user/chat")
                         .header(AUTHORIZATION, USER_TOKEN)
                         .param("chatName", "null")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isEmpty());
+    }
+
+    @Test
+    @ApiOperation(value = "Проверка на возвращение single чатов авторизованного пользователя")
+    @Sql(scripts = "/script/TestChatResourceController/TestGetSingleChatDto/Before.sql",
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(scripts = "/script/TestChatResourceController/TestGetSingleChatDto/After.sql",
+            executionPhase = AFTER_TEST_METHOD)
+    public void getSingleChatDto() throws Exception {
+
+        String USER_TOKEN = getToken("test101@mail.ru", "123");
+
+        this.mvc.perform(get("/api/user/chat/single")
+                        .header(AUTHORIZATION, USER_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id", Is.is(113)))
+                .andExpect(jsonPath("$[0].name", Is.is("third")))
+                .andExpect(jsonPath("$[0].image", Is.is("third_user_image")))
+                .andExpect(jsonPath("$[0].lastMessage", Is.is("2023-01-11_last_message_third_chat")))
+                .andExpect(jsonPath("$[0].persistDateTimeLastMessage", Is.is("2023-01-11T00:00:00")))
+
+                .andExpect(jsonPath("$[1].id", Is.is(114)))
+                .andExpect(jsonPath("$[1].name", Is.is("second")))
+                .andExpect(jsonPath("$[1].image", Is.is("second_user_image")))
+                .andExpect(jsonPath("$[1].lastMessage", Is.is("2023-02-11_last_message_four_chat")))
+                .andExpect(jsonPath("$[1].persistDateTimeLastMessage", Is.is("2023-02-11T00:00:00")))
+
+                .andExpect(jsonPath("$.length()", Is.is(2)));
+    }
+
+    @Test
+    @ApiOperation(value = "Проверка на возвращение пустого массива")
+    @Sql(scripts = "/script/TestChatResourceController/TestGetSingleChatDtoEmpty/Before.sql",
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(scripts = "/script/TestChatResourceController/TestGetSingleChatDtoEmpty/After.sql",
+            executionPhase = AFTER_TEST_METHOD)
+    public void getSingleChatDtoEmpty() throws Exception {
+
+        String USER_TOKEN = getToken("test101@mail.ru", "123");
+
+        this.mvc.perform(get("/api/user/chat/single")
+                        .header(AUTHORIZATION, USER_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$").isEmpty());
+    }
+
+    @Test
+    @ApiOperation(value = "Проверка на возвращение group чатов авторизованного пользователя")
+    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDto/Before.sql",
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDto/After.sql",
+            executionPhase = AFTER_TEST_METHOD)
+    public void getGroupChatDto() throws Exception {
+
+        String USER_TOKEN = getToken("test101@mail.ru", "123");
+
+        this.mvc.perform(get("/api/user/chat/group")
+                        .header(AUTHORIZATION, USER_TOKEN)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].id", Is.is(111)))
+                .andExpect(jsonPath("$[0].chatName", Is.is("first_chat")))
+                .andExpect(jsonPath("$[0].lastMessage", Is.is("2023-01-03_last_message_first_chat")))
+                .andExpect(jsonPath("$[0].image", Is.is("first_image")))
+                .andExpect(jsonPath("$[0].persistDateTime", Is.is("2000-01-01T00:00:00")))
+
+                .andExpect(jsonPath("$[1].id", Is.is(112)))
+                .andExpect(jsonPath("$[1].chatName", Is.is("second_chat")))
+                .andExpect(jsonPath("$[1].lastMessage", Is.is("2023-01-05_last_message_second_chat")))
+                .andExpect(jsonPath("$[1].image", Is.is("second_image")))
+                .andExpect(jsonPath("$[1].persistDateTime", Is.is("2000-02-01T00:00:00")))
+
+                .andExpect(jsonPath("$.length()", Is.is(2)));
+    }
+
+    @Test
+    @ApiOperation(value = "Проверка на возвращение пустого массива")
+    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDtoEmpty/Before.sql",
+            executionPhase = BEFORE_TEST_METHOD)
+    @Sql(scripts = "/script/TestChatResourceController/TestGetGroupChatDtoEmpty/After.sql",
+            executionPhase = AFTER_TEST_METHOD)
+    public void getGroupChatDtoEmpty() throws Exception {
+
+        String USER_TOKEN = getToken("test101@mail.ru", "123");
+
+        this.mvc.perform(get("/api/user/chat/group")
+                        .header(AUTHORIZATION, USER_TOKEN)
                         .contentType(MediaType.APPLICATION_JSON))
                 .andDo(print())
                 .andExpect(status().isOk())
