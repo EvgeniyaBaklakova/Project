bc76c75fd1e4e6f6440d1c0c9445a80f925fdc70
EvgeniyaBaklakova <121979488+EvgeniyaBaklakova@users.noreply.github.com>
2023-07-31 19:46:59 +0300

    Commit "AddUserByIdToGroupChat" correct two

--- src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ChatResourceController.java
+++ src/main/java/com/javamentor/qa/platform/webapp/controllers/rest/ChatResourceController.java
@@ -5,14 +5,22 @@ import com.javamentor.qa.platform.models.dto.chat.GroupChatDto;
 import com.javamentor.qa.platform.models.dto.chat.SingleChatDto;
 import com.javamentor.qa.platform.models.entity.user.User;
 import com.javamentor.qa.platform.service.abstracts.dto.ChatDtoService;
-import com.javamentor.qa.platform.service.impl.model.GroupChatServiceImpl;
+import com.javamentor.qa.platform.service.abstracts.model.GroupChatService;
+import com.javamentor.qa.platform.service.abstracts.model.SingleChatService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 
 @Api(value = "Контроллер для работы с чатами пользователя")
@@ -20,11 +28,13 @@ import java.util.List;
 @RequestMapping("/api/user/chat")
 public class ChatResourceController {
     private final ChatDtoService chatDtoService;
-    private final GroupChatServiceImpl groupChatService;
+    private final GroupChatService groupChatService;
+    private final SingleChatService singleChatService;
 
-    public ChatResourceController(ChatDtoService chatDtoService, GroupChatServiceImpl groupChatService) {
+    public ChatResourceController(ChatDtoService chatDtoService, GroupChatService groupChatService, SingleChatService singleChatService) {
         this.chatDtoService = chatDtoService;
         this.groupChatService = groupChatService;
+        this.singleChatService = singleChatService;
     }
 
     @GetMapping
@@ -51,8 +61,19 @@ public class ChatResourceController {
 
     @PostMapping("/group/{id}/join")
     @ApiOperation(value = "Добавление пользователя по id в групповой чат")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Успешный запрос!"),
+            @ApiResponse(code = 201, message = "Пользователь добавлен!"),
+            @ApiResponse(code = 401, message = "Вы не авторизованы для просмотра ресурса"),
+            @ApiResponse(code = 403, message = "Доступ к ресурсу, к которому вы пытались обратиться, запрещен"),
+            @ApiResponse(code = 404, message = "Пользователь или чат не найдены!")})
     public ResponseEntity<HttpStatus> addUserToChat(@PathVariable("id") Long groupChatId, @RequestBody Long userId) {
         groupChatService.addUserByIdToGroupChat(groupChatId, userId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    @PostMapping("/single")
+    public ResponseEntity<HttpStatus> createSingleChat(@RequestParam Long userId, @RequestParam String message, @AuthenticationPrincipal User userSender){
+        singleChatService.createSingleChat(userId, message, userSender);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
